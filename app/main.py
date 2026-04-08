02d0e9fcc104782cecfcc7cd57b18fd2b85e5fe3
Maksym Kobelianskyi <borderfine@gmail.com>
2026-04-08 18:09:31 +0300

    Update submodule py-linter-formatter to include fixes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return {
+"line": error["line_number"],
+"column": error["column_number"],
+"message": error["text"],
+"name": error["code"],
+"source": "flake8",
+}
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    formatted_errors = [format_linter_error(err) for err in errors]
+    status = "passed" if len(formatted_errors) == 0 else "failed"
+    return {"errors": formatted_errors, "path": file_path, "status": status}
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return [format_single_linter_file(path, errs) for path, errs in linter_report.items()]
     pass
